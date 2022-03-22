66dea4a09b027a4a34eea3e34700e2bc140f6143
thaihc <thaisherculanodecarvalho@gmail.com>
2022-03-21 23:05:13 -0300

    Chamando DAO no controller

--- src/controllers/estoque-controller.js
+++ src/controllers/estoque-controller.js
@@ -1,25 +1,30 @@
+import EstoqueDAO from "../DAO/estoqueDAO.js";
 import Estoque from "../models/estoque.js";
 
 const estoqueController = (app, bd) => {
-  app.get("/estoque", (req, res) => {
-    const todoEstoque = bd.estoqueOk;
+  const estoqueDAO = new EstoqueDAO(bd);
 
-    res.json({
-      " estoqueOk": todoEstoque,
-      erro: false,
-    });
+  app.get("/estoque", (req, res) => {
+    estoqueDAO
+      .acessaTodoEstoque()
+      .then((resposta) => {
+        res.json(resposta);
+      })
+      .catch((erro) => {
+        res.json(erro);
+      });
   });
 
   app.get("/estoque/nome/:nome", (req, res) => {
     const nome = req.params.nome;
-    const produtoEncontrado = bd.estoqueOk.filter(
-      (estoque) => estoque.nome == nome
-    );
-
-    res.json({
-      estoque: produtoEncontrado,
-      erro: false,
-    });
+    estoqueDAO
+      .acessaUmAtributo(nome)
+      .then((resposta) => {
+        res.json(resposta);
+      })
+      .catch((erro) => {
+        res.json(erro);
+      });
   });
 
   app.post("/estoque", (req, res) => {
@@ -34,13 +39,57 @@ const estoqueController = (app, bd) => {
         body.tipo
       );
 
-      bd.estoqueOk.push(novoProduto);
-
+      estoqueDAO
+        .insereItem(novoItem)
+        .then((resposta) => {
+          res.json(resposta);
+        })
+        .catch((erro) => {
+          res.json(erro);
+        });
+    } catch (error) {
       res.json({
-        msg: `Produto ${novoProduto.nome} adicionado ao estoque`,
-        estoqueOk: novoProduto,
-        erro: false,
+        msg: error.message,
+        erro: true,
       });
+    }
+  });
+
+  app.delete("/estoque/id/:id", (req, res) => {
+    const nome = req.params.id;
+
+    tarefaDAO
+      .deletaItem(id)
+      .then((resposta) => {
+        res.json(resposta);
+      })
+      .catch((erro) => {
+        res.json(erro);
+      });
+  });
+
+  app.put("/estoque/id/:id", (req, res) => {
+    const nome = req.params.id;
+
+    const body = req.body;
+
+    try {
+      const estoqueAtualizado = new Estoque(
+        body.id,
+        body.nome,
+        body.preco,
+        body.quantidade,
+        body.tipo
+      );
+
+      estoqueDAO
+        .atualizaItem(id, estoqueAtualizado)
+        .then((resposta) => {
+          res.json(resposta);
+        })
+        .catch((erro) => {
+          res.json(erro);
+        });
     } catch (error) {
       res.json({
         msg: error.message,
@@ -49,51 +98,5 @@ const estoqueController = (app, bd) => {
     }
   });
 };
-
-app.delete("/estoque/nome/:nome", (req, res) => {
-  const nome = req.params.nome;
-
-  const novoBD = bd.estoqueOk.filter((estoque) => estoque.nome !== nome);
-  bd.estoqueOk = novoBD;
-
-  res.json({
-    msg: `Produto ${nome} excluido do estoque com sucesso!`,
-    erro: false,
-  });
-});
-
-app.put("/estoque/nome/:nome", (req, res) => {
-  const nome = req.params.nome;
-
-  const body = req.body;
-
-  try {
-    const estoqueAtualizado = new Estoque(
-      body.id,
-      body.nome,
-      body.preco,
-      body.quantidade,
-      body.tipo
-    );
-
-    bd.estoqueOk = bd.estoqueOk.map((estoque) => {
-      if (estoque.nome === nome) {
-        return estoqueAtualizado;
-      }
-      return estoque;
-    });
-
-    res.json({
-      msg: `Produto ${estoqueAtualizado.nome} atualizado no estoque com sucesso`,
-      estoque: estoqueAtualizado,
-      erro: false,
-    });
-  } catch (error) {
-    res.json({
-      msg: error.message,
-      erro: true,
-    });
-  }
-});
 
 export default estoqueController;
